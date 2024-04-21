0321e763db5d0a05a0e705b4b324628d5912c887
gusgonza <19624236-gusgonza@users.noreply.gitlab.com>
2024-04-21 16:12:30 +0200

    update generator

--- M03-PROG/pythonProject/generatorData.py
+++ M03-PROG/pythonProject/generatorData.py
@@ -7,7 +7,8 @@ fake = Faker('es_ES')
 
 
 def escribir_csv(datos, nombre_archivo, headers):
-    ruta_carpeta = os.path.join(os.getcwd(), 'datos_generados_csv')  # Reemplaza 'carpeta_csv' con el nombre de tu carpeta
+    ruta_carpeta = os.path.join(os.getcwd(),
+                                'datos_generados_csv')  # Reemplaza 'carpeta_csv' con el nombre de tu carpeta
     os.makedirs(ruta_carpeta, exist_ok=True)  # Crear la carpeta si no existe
     ruta_archivo = os.path.join(ruta_carpeta, nombre_archivo)
     with open(ruta_archivo, mode='w', newline='', encoding='utf-8') as file:
@@ -43,7 +44,6 @@ def generar_telefono():
     # Comprobar si el número de teléfono ya ha sido generado
     while telefono in telefonos_generados:
         telefono = f'{random.randint(600000000, 699999999)}'
-    # Añadir el número de teléfono al conjunto de números generados
     telefonos_generados.add(telefono)
     return telefono
 
@@ -76,7 +76,7 @@ def generar_datos_camareros(num_datos):
     return datos
 
 
-# generar datos aleatorios para la tabal mesa
+# generar datos aleatorios para la tabla mesa
 def generar_datos_mesas(num_datos):
     datos = []
     for i in range(num_datos):
@@ -130,10 +130,11 @@ def generar_datos_comandas(num_datos):
 
 # Generar datos aleatorios para la tabla de menús
 def generar_datos_menus(num_datos):
+    menus_lista = ['Nebulosas del Sabor', 'Ruta Galáctica', 'La Nasa', 'Sinfonía de Sabores', 'Viaje Cósmico']
     datos = []
     for i in range(num_datos):
         id = i + 1
-        nombre_menu = fake.word()
+        nombre_menu = random.choice(menus_lista)
         descripcion = fake.sentence()
         precio = round(random.uniform(5, 50), 2)
 
@@ -145,10 +146,12 @@ def generar_datos_menus(num_datos):
 # Generar datos aleatorios para la tabla intermedia entre menu y comanda
 def generar_datos_menu_comanda(num_menus, num_comandas):
     datos = []
+    alergenos_lista = ['gluten', 'crustáceos', 'huevos', 'pescado', 'cacahuetes', 'soja', 'leche', 'frutos de cáscara', 'apio', 'mostaza', 'sésamo', 'sulfitos', 'moluscos']
+
     for menu_id in range(1, num_menus + 1):
         comandas_seleccionadas = random.sample(range(1, num_comandas + 1), random.randint(1, 5))
         for comanda_id in comandas_seleccionadas:
-            alergenos = fake.sentence()
+            alergenos = ', '.join(random.sample(alergenos_lista, random.randint(1, 4)))
             datos.append((menu_id, comanda_id, alergenos))
 
     return datos
@@ -156,13 +159,14 @@ def generar_datos_menu_comanda(num_menus, num_comandas):
 
 # Generar datos aleatorios para la tabla de platos
 def generar_datos_platos(num_datos):
+    platos_referentes = ['Crema de', 'Leche de', 'Queso de', 'Mantequilla de', 'Nata de', 'Yogur de', 'Helado de', 'Espuma de']
+
     datos = []
     for i in range(num_datos):
         id = i + 1
-        nombre_plato = fake.word()
+        nombre_plato = random.choice(platos_referentes) + " " + fake.word()
         descripcion = fake.sentence()
-        id_chef = random.randint(1, num_datos)  # asumiendo que hay el mismo número de chefs que de platos
-
+        id_chef = random.randint(1, num_datos)
         datos.append((id, nombre_plato, descripcion, id_chef))
 
     return datos
@@ -182,13 +186,26 @@ def generar_datos_menus_platos(num_menus, num_platos):
 # Generar datos aleatorios para la tabla de productos
 def generar_datos_productos(num_datos):
     proveedores = ['Rungis Market', 'Miyazaki Wagyu', 'Tsar Nicoulai Caviar']
+    productos_referentes = ['Foie Gras', 'Trufa Negra', 'Carrillera de Buey Wagyu', 'Caviar Beluga', 'Ostras Gillardeau', 'Salmón Salvaje de Alaska', 'Langosta de Maine', 'Kobe Beef', 'Cangrejo Real', 'Caracoles de Borgoña']
     datos = []
-    for _ in range(num_datos):
-        nombre = fake.word()
-        stock = random.randint(0, 100)
+    for i in range(num_datos):
+        id_producto = i + 1
+        nombre = random.choice(productos_referentes) + " " + fake.word()
+        stock = random.randint(1, 100)
         proveedor = random.choice(proveedores)
 
-        datos.append((nombre, stock, proveedor))
+        datos.append((id_producto, nombre, stock, proveedor))
+
+    return datos
+
+
+# Generar datos aleatorios para la tabla intermedia entre platos y productos
+def generar_datos_platos_productos(num_platos, num_productos):
+    datos = []
+    for plato_id in range(1, num_platos + 1):
+        productos_seleccionados = random.sample(range(1, num_productos + 1), random.randint(1, 5))
+        for producto_id in productos_seleccionados:
+            datos.append((plato_id, producto_id))
 
     return datos
 
@@ -196,44 +213,52 @@ def generar_datos_productos(num_datos):
 # Escribir los datos en archivos CSV
 headers_usuarios = ['id', 'nombre', 'apellido1', 'apellido2', 'fecha_nacimiento', 'email', 'fecha_registro', 'telefono']
 datos_usuarios = generar_datos_usuarios(25)
-escribir_csv(datos_usuarios, 'Usuarios.csv', headers_usuarios)
+escribir_csv(datos_usuarios, 'usuarios.csv', headers_usuarios)
 
-headers_chefs = ['id', 'salario', 'fecha_contratacion', 'disponible']
+headers_chefs = ['id_usuario', 'salario', 'fecha_contratacion', 'disponible']
 datos_chefs = generar_datos_chefs(25)
-escribir_csv(datos_chefs, 'Chefs.csv', headers_chefs)
+escribir_csv(datos_chefs, 'chefs.csv', headers_chefs)
 
-headers_camareros = ['id', 'salario', 'fecha_contratacion', 'disponibilidad']
+headers_camareros = ['id_usuario', 'salario', 'fecha_contratacion', 'disponibilidad']
 datos_camareros = generar_datos_camareros(25)
-escribir_csv(datos_camareros, 'Camareros.csv', headers_camareros)
+escribir_csv(datos_camareros, 'camareros.csv', headers_camareros)
 
-headers_mesas = ['id', 'num_mesa', 'id_camarero', 'fecha_registro']
+headers_mesas = ['num_mesa', 'id_camarero', 'fecha_registro', 'disponibilidad']
 datos_mesas = generar_datos_mesas(25)
-escribir_csv(datos_mesas, 'Mesas.csv', headers_mesas)
+escribir_csv(datos_mesas, 'mesas.csv', headers_mesas)
 
-headers_clientes = ['id', 'comensales', 'fecha_reserva']
+headers_clientes = ['id_usuario', 'comensales', 'fecha_reserva']
 datos_clientes = generar_datos_clientes(25)
-escribir_csv(datos_clientes, 'Clientes.csv', headers_clientes)
+escribir_csv(datos_clientes, 'clientes.csv', headers_clientes)
 
-headers_clientes_mesas = ['id', 'id_cliente', 'id_mesa', 'fecha_reserva']
+headers_clientes_mesas = ['id_usuario', 'num_mesa', 'fecha_reserva']
 datos_clientes_mesas = generar_datos_cliente_mesa(25, 25)
-escribir_csv(datos_clientes_mesas, 'Clientes_Mesas.csv', headers_clientes_mesas)
+escribir_csv(datos_clientes_mesas, 'clientes_mesas.csv', headers_clientes_mesas)
 
-headers_comandas = ['id', 'fecha', 'pagado', 'num_mesa']
+headers_comandas = ['id_comanda', 'fecha', 'pagado', 'num_mesa']
 datos_comandas = generar_datos_comandas(25)
-escribir_csv(datos_comandas, 'Comandas.csv', headers_comandas)
+escribir_csv(datos_comandas, 'comandas.csv', headers_comandas)
 
-headers_platos = ['id', 'nombre_plato', 'descripcion', 'id_chef']
-datos_platos = generar_datos_platos(25)
-escribir_csv(datos_platos, 'Platos.csv', headers_platos)
-
-headers_productos = ['id', 'nombre', 'stock', 'proveedor']
-datos_productos = generar_datos_productos(25)
-escribir_csv(datos_productos, 'Ingredientes.csv', headers_productos)
-
-headers_menus = ['id', 'nombre_menu', 'descripcion', 'precio']
+headers_menus = ['id_menu', 'nombre_menu', 'descripcion', 'precio']
 datos_menus = generar_datos_menus(25)
-escribir_csv(datos_menus, 'Menus.csv', headers_menus)
+escribir_csv(datos_menus, 'menus.csv', headers_menus)
 
-headers_menus_platos = ['id', 'id_menu', 'id_plato']
+headers_menus_comandas = ['id_menu', 'id_comanda', 'alergenos']
+datos_menus_comandas = generar_datos_menu_comanda(25, 25)
+escribir_csv(datos_menus_comandas, 'menus_comandas.csv', headers_menus_comandas)
+
+headers_platos = ['id_plato', 'nombre_plato', 'descripcion', 'id_chef']
+datos_platos = generar_datos_platos(25)
+escribir_csv(datos_platos, 'platos.csv', headers_platos)
+
+headers_menus_platos = ['id_menu', 'id_plato']
 datos_menus_platos = generar_datos_menus_platos(25, 25)
-escribir_csv(datos_menus_platos, 'Menus_Platos.csv', headers_menus_platos)
+escribir_csv(datos_menus_platos, 'menus_platos.csv', headers_menus_platos)
+
+headers_productos = ['id_producto', 'nombre', 'stock', 'proveedor']
+datos_productos = generar_datos_productos(25)
+escribir_csv(datos_productos, 'productos.csv', headers_productos)
+
+headers_platos_productos = ['id_plato', 'id_producto']
+datos_platos_productos = generar_datos_platos_productos(25, 25)
+escribir_csv(datos_platos_productos, 'platos_productos.csv', headers_platos_productos)
